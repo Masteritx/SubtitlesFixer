1c433a748f1d3efd255307a75a2061e0621fb8fb
Shahar Zelig <shahar@zelig.me>
2018-03-28 18:40:15 +0300

    Changing to functions without main logic.

--- coverter.py
+++ coverter.py
@@ -1,7 +1,43 @@
-PATH = r"F:\Subs\Inglourious.Basterds.2009.720p.BRRip.1.4GB.MkvCage-heb.srt"
+import argparse
 
-with open(PATH) as f:
-    data = f.read()
+READ_MODE = "r"
 
-with open(r"D:\Desktop\try.txt", "w+") as f:
-    f.write(data.decode("cp1255").encode("utf8"))
+def fix_wrongly_encoded_data(data):
+    try:
+        return data.decode("cp1255").encode("utf8")
+
+    except Exception as e:
+        print "[-] Could not fix encoded subtitles."
+        print "[-] Exception: %s", data
+        return False
+
+def write_to_file(path, data, mode="w+"):
+    if "a" not in mode and "w" not in mode or "b" in mode:
+        print "[-] The mode {0} is not valid.".format(mode)
+        return False
+
+    print "[+] Got valid mode {0}".format(mode)
+
+    try:
+        with open(path, mode) as f:
+            f.write(data)
+
+    except Exception as e:
+        print "[-] Could not open file {0} in mode {1}".format(path, mode)
+        print "[-] Exception: {0}".format(e)
+        return False
+
+    return True
+
+def read_file(path):
+    try:
+        print "[+] Trying openning file %s", path
+        with open(path, READ_MODE) as f:
+            data = f.read()
+
+    except Exception as e:
+        print "[-] Could not open path %s", path
+        print "[-] Exception: %s", e
+        return False
+    print "[+} Opened file %s successfully", path
+    return data
